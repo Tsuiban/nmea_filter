8dfc9efe16e97acf70af3bb20378c5ee6ed8857e
Kevin-Neil Klop <kevin.klop@kevinklop.ca>
2023-06-08 07:46:12 -0700

    Added a few more filter functions to exclude certain talkers or include certain talkers.  This test is done after all other tests are performed.
    Also changed the option of include/exclude messages to be include-messages or exclude-messages as well as changing the short option names.

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,57 @@ use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 use chrono::NaiveTime;
 
+/// This program filters an NMEA-0183 stream.  It filters out unwanted NMEA-0183 sentences
+/// by sentence name or time or else merely removes invalid sentences.
+///
+/// For both --include and --exclude, the argument is a LIST of sentences.   For example
+/// --include GLL,GGA,ZDA or --exclude XDR,HDG
+///
+/// --start and --end both take a time in the form of hh:mm:ss.sss.
 #[derive(Debug, Parser)]
 struct Cli {
 	filenames : Option<Vec<PathBuf>>,
 
-	#[arg(long, short, value_delimiter=',')]
-	include : Option<Vec<String>>,
+	/// Pass through all sentences that occur on or before this time
+	#[arg(long="end", short='E', default_value="23:59:59.999")]
+	end_time : NaiveTime,
 
-	#[arg(long, short, value_delimiter=',')]
-	exclude : Option<Vec<String>>,
+	/// Pass through these sentences
+	#[arg(long, short='m', value_delimiter=',')]
+	include_messages: Option<Vec<String>>,
 
+	/// Pass through all sentences that are NOT these sentences.
+	#[arg(long, short='M', value_delimiter=',')]
+	exclude_messages: Option<Vec<String>>,
+
+	/// Pass through all sentences that occur on or after this time
 	#[arg(long="start", short='S', default_value="00:00:00")]
 	start_time : NaiveTime,
 
-	#[arg(long="end", short='E', default_value="23:59:59.999")]
-	end_time : NaiveTime,
+	/// Pass through sentences only from these talkers
+	#[arg(long, short='t', value_delimiter=',')]
+	include_talkers : Option<Vec<String>>,
+
+	/// Excluse sentences from these talkers
+	#[arg(short='T', long, value_delimiter=',')]
+	exclude_talkers : Option<Vec<String>>,
+
+	/// List the unique messages that are in the file
+	#[arg(short='u', long)]
+	list_unique_messages : bool,
+
+	/// List the unique talkers that are in the file
+	#[arg(short='U', long)]
+	list_unique_talkers : bool,
+
+	/// Pass through all valid sentences
+	#[arg(long,short='V')]
+	valid : bool,
 }
 	  
 fn main() {
 	let cli = Cli::parse();
+
 	if cli.filenames.is_some() {
 		for f in &cli.filenames.clone().unwrap() {
 			match File::open(f) {
@@ -37,11 +69,21 @@ fn main() {
 }
 
 fn process_file(reader : &mut dyn BufRead, cli : &Cli) {
+	let mut unique_messages : Vec<String> = Vec::new();
+	let mut unique_talkers : Vec<String> = Vec::new();
 	let mut current_time = NaiveTime::default();
 	for line in reader.lines() {
 		if let Ok(line) = line {
 			let sentence = NmeaBaseSentence::from(&line);
-			if !sentence.message_type().is_empty() {
+			let talker = sentence.sender();
+			let message_type = sentence.message_type();
+			if cli.list_unique_talkers && !unique_talkers.contains(&talker) {
+				unique_talkers.push(talker.clone());
+			}
+			if cli.list_unique_messages && !unique_messages.contains(&message_type) {
+				unique_messages.push(message_type.clone());
+			}
+			if sentence.is_valid() {
 				let mut new_time = None;
 				match sentence.message_type().as_str() {
 					"BWC" => new_time = sentence.get_time(0),
@@ -66,22 +108,49 @@ fn process_file(reader : &mut dyn BufRead, cli : &Cli) {
 				if let Some(new_time) = new_time {
 					current_time = new_time;
 				}
-				println!("{:?} -- {:?} -- {:?}", &cli.start_time, &current_time, &cli.end_time);
+				//println!("{:?} -- {:?} -- {:?}", &cli.start_time, &current_time, &cli.end_time);
 				
 				if current_time >= cli.start_time && current_time <= cli.end_time {
-					if is_in(&cli.include, &sentence.message_type()) {
-						pass_through(&line);
-					} else if cli.exclude.is_some() && !is_in(&cli.exclude, &sentence.message_type()) {
-						pass_through(&line);
+					if is_in(&cli.include_messages, &sentence.message_type()) {
+							pass_through(&cli, &sentence.sender(), &line);
+					} else if !cli.exclude_messages.is_some() || !is_in(&cli.exclude_messages, &sentence.message_type()) {
+						pass_through(&cli, &sentence.sender(), &line);
+					} else if cli.valid {
+						pass_through(&cli, &sentence.sender(), &line);
 					}
 				}
 			}
 		}
 	}
+	if cli.list_unique_talkers {
+		unique_talkers.sort();
+		println!("Unique Talkers: {:?}", unique_talkers)
+	}
+
+	if cli.list_unique_messages {
+		unique_messages.sort();
+		println!("Unique messages: {:?}", unique_messages)
+	}
 }
 
-fn pass_through(line : &String) {
-	println!("{}", line);
+fn pass_through(cli : &Cli, sender : &String, line : &String) {
+	if (
+		cli.exclude_talkers.is_some() &&
+			!cli
+				.exclude_talkers
+				.as_ref()
+				.unwrap()
+				.contains(sender)
+	) || (
+		cli.include_talkers.is_some() &&
+			cli
+				.include_talkers
+				.as_ref()
+				.unwrap()
+				.contains(sender)
+	) {
+		println!("{}", line);
+	}
 }
 
 
